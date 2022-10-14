a94f7de07eee4cf5a0859b40b5b1e0462b95d97e
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-14 13:48:55 +0300

    Changed example 1

--- hw1/example1/Program.cs
+++ hw1/example1/Program.cs
@@ -5,6 +5,9 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2) Console.WriteLine($"Число {num1} больше, чем число {num2}.");
-if (num1 < num2) Console.WriteLine($"Число {num1} меньше, чем число {num2}.");
 if (num1 == num2) Console.WriteLine($"Число {num1} равно числу {num2}.");
+else
+{
+    if (num1 > num2) Console.WriteLine($"Число {num1} больше, чем число {num2}.");
+    else Console.WriteLine($"Число {num1} меньше, чем число {num2}.");
+}
