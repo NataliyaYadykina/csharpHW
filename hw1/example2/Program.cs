a2f69dda19cf62da2747b3ca3d45e23927ef2736
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-14 14:17:23 +0300

    Add example 2

---
+++ hw1/example2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine($"Среди чисел {a}, {b}, {c} максимальным числом является: {max}.");
