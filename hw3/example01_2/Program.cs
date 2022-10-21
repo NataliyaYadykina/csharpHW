2bdd456b8927bf77b3ef441b95c08172225f49c9
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-21 14:57:12 +0300

    Add example 01_2

---
+++ hw3/example01_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// Вывести, является ли число палиндромом
+void PrintAboutNumber(int number)
+{
+    int min = 9999;
+    int max = 100000;
+    int countSymbolNumber = 5;
+
+    if((number > min && number < max) || (-1 * number > min && -1 * number < max))
+    {
+        int tempNumber = number;
+        double reverseNumber = 0;
+
+        while (tempNumber > 0)
+        {
+            reverseNumber = reverseNumber + tempNumber % 10 * Math.Pow(10, countSymbolNumber - 1);
+            //Console.WriteLine(reverseNumber);
+            countSymbolNumber--;
+            tempNumber = tempNumber / 10;
+        }
+
+        if (number == (int)reverseNumber && number > 0)
+        {
+            Console.WriteLine("Введенное число - палиндром!");
+        }
+        else Console.WriteLine("Введенное число не является палиндромом!");
+
+    }
+    else Console.WriteLine("Введено не пятизначное число!");
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintAboutNumber(number);
