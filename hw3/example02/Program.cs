a49ea2290577e4ad97da8b383a9981f0f8702c31
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-21 15:06:51 +0300

    Add example 02

---
+++ hw3/example02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+// Найти расстояние между точками в 3D пространстве
+double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return distance;
+}
+
+Console.Write("Введите координаты первой точки - x, y, z: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты второй точки - x, y, z: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = GetDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(distance);
