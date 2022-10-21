705084bc61ac4cc26d002f24488a62a0c9a0f1ad
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-21 15:13:09 +0300

    Add example 03

---
+++ hw3/example03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// Получаем таблицу кубов
+void PrintSquaresToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintSquaresToConsole(n);
