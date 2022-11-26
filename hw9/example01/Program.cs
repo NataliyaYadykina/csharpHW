e8a9fe89e36020ad1863da048728a075b32dc1a4
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-26 17:47:26 +0300

    Add example 01 - Recursive: print numbers from N to 1

---
+++ hw9/example01/Program.cs
@@ -0,0 +1,22 @@
+﻿// 64: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5-> "5, 4, 3, 2, 1"
+// N = 8-> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int PrintNumbers(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int tempResult = n;
+    Console.WriteLine(tempResult);
+    return PrintNumbers(n - 1);
+}
+
+Console.Write("Write N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(n));
